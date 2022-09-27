c264d72a3221ae87fb30ee3068cd2f3255cea2a5
evanhuang117 <evanhuang117@gmail.com>
2022-09-27 13:41:11 -0400

    fix es upsert

--- scraper/etsy_scraper.py
+++ scraper/etsy_scraper.py
@@ -1,6 +1,5 @@
 from elasticsearch.exceptions import ConnectionError
 from elasticsearch import AsyncElasticsearch
-from elasticsearch.helpers import async_bulk
 import asyncio
 import aiohttp
 from logger import logger
@@ -18,7 +17,7 @@ async def main():
         except ConnectionError:
             await asyncio.sleep(2)
 
-    print('connected')
+    logger.info('connected')
     session = aiohttp.ClientSession()
 
     await do_stuff_periodically(
@@ -27,33 +26,56 @@ async def main():
 
 async def update_elasticsearch(es, session):
     async for listings in get_listings(session):
-        # replace newline chars in text bc bulk uses new line delimited json
-        listings = [{key: value.replace('\n', '') for key, value in l.items() if isinstance(value, str)} for l in listings]
-        print(intersperse(listings, {'create'})[:2])
-        resp = await es.bulk(body=intersperse(listings, {'create':{}})[:2])
-        print(resp)
+        bulk_actions = await process_listings(listings)
+        logger.debug(bulk_actions[:2])
+        resp = await es.bulk(body=bulk_actions)
+        logger.debug(f"ES ERRORS: {resp.get('errors')}")
+    logger.info('finished updating es')
 
 
-def intersperse(arr, item):
-    result = [item] * (len(arr) * 2)
-    result[1::2] = arr
+async def process_listings(listings):
+    # create es actions and ids for listings
+    actions = [{'update': {'_id': f"{l.get('listing_id')}{l.get('user_id')}{l.get('shop_id')}",
+                           '_index': 'etsy'}}
+               for l in listings]
+    # replace newline chars in text bc bulk uses new line delimited json
+    listings = [{key: value.replace('\n', '') for key, value in l.items(
+    ) if isinstance(value, str)} for l in listings]
+    # create docs to upsert
+    # add upsert=true to all results
+    listings = [{'doc': l, 'doc_as_upsert': 'true'} for l in listings]
+    return await interleave(actions, listings)
+
+
+"""
+interleaves two arrays with the same length, with the first element of arr1 as the first element
+"""
+
+
+async def interleave(arr1, arr2):
+    result = arr1 + arr2
+    result[::2] = arr1
+    result[1::2] = arr2
     return result
 
 
 async def get_listings(session, params={'limit': 100, 'offset': 0}, search_string="", headers={'x-api-key': etsy_api_key}):
+    debug = True
     while True:
         async with session.get(f"https://openapi.etsy.com/v3/application/listings/active", params={str(i): str(j) for i, j in params.items()}, headers={str(i): str(j) for i, j in headers.items()}) as response:
             if (response.status != 200):
-                print(response)
-                raise StopAsyncIteration()
+                logger.debug(response)
+                return
             page = await response.json()
         yield page.get('results')
         params['offset'] += 100
+        if debug:
+            break
 
 
 async def do_stuff_periodically(interval, periodic_function, *args):
     while True:
-        print("Starting periodic function")
+        logger.debug("Starting periodic function")
         await asyncio.gather(
             asyncio.sleep(interval),
             periodic_function(*args),
--- scraper/logger.py
+++ scraper/logger.py
@@ -3,13 +3,24 @@ import os
 
 
 def setup_logger():
-    # log_dir = os.path.dirname(os.path.abspath(__file__)) + "/logs/"
-    # if not os.path.exists(log_dir):
-    #     os.makedirs(log_dir)
+    log_dir = os.path.dirname(os.path.abspath(__file__)) + "/logs/"
+    if not os.path.exists(log_dir):
+        os.makedirs(log_dir)
 
     # logging.basicConfig(filename=log_dir + "gpu-scraper.log", filemode='w', level=logging.DEBUG,
     #                     format='%(asctime)s %(levelname)s %(name)s %(message)s')
     logger = logging.getLogger(__name__)
+    logFormatter = logging.Formatter(
+        "%(asctime)s [%(threadName)-12.12s] [%(levelname)-5.5s]  %(message)s")
+
+    fileHandler = logging.FileHandler(log_dir + "scraper.log")
+    fileHandler.setFormatter(logFormatter)
+    logger.addHandler(fileHandler)
+
+    consoleHandler = logging.StreamHandler()
+    consoleHandler.setFormatter(logFormatter)
+    logger.addHandler(consoleHandler)
+    logger.setLevel(logging.DEBUG)
 
     return logger
 
